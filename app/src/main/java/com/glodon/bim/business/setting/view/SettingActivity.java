fbc67de035ff6a9fa8b31e15634c34e24b3fe81f
1F-2-3-14  周瑞峰(10024029) <zhourf@glodon.com>
2017-11-28 11:14:46 +0800

    修改账户设置的电话号码

--- app/src/main/java/com/glodon/bim/business/setting/view/SettingActivity.java
+++ app/src/main/java/com/glodon/bim/business/setting/view/SettingActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -121,9 +122,11 @@ public class SettingActivity extends BaseActivity implements SettingContract.Vie
      * @param phone 电话号码
      */
     private void call(String phone) {
-        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phone));
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        if(!TextUtils.isEmpty(phone)) {
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 
     @Override
