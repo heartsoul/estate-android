93e3dfe3d8e05ca85221656273a0588cb8c7beb1
1F-2-3-14  周瑞峰(10024029) <zhourf@glodon.com>
2018-02-27 14:47:25 +0800

    优化

--- app/src/main/java/com/glodon/bim/business/login/view/LoginActivity.java
+++ app/src/main/java/com/glodon/bim/business/login/view/LoginActivity.java
@@ -173,8 +173,8 @@ public class LoginActivity extends BaseActivity implements LoginContract.View, V
         String password = SharedPreferencesUtil.getString(CommonConfig.PASSWORD, "");
         mUsernameEt.setText(username);
         mPasswordEt.setText(password);
-            mUsernameEt.setText("15822320523");//徐园
-            mPasswordEt.setText("123qwe");
+//            mUsernameEt.setText("15822320523");//徐园
+//            mPasswordEt.setText("123qwe");
 //        mUsernameEt.setText("15001340978");
 //        mPasswordEt.setText("123qwe");
 //        mUsernameEt.setText("15733178348");//贾光花
