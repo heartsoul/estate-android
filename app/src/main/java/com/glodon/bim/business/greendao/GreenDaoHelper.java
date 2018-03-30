deb78fed940cc55e792c0533132d744b019439b4
周瑞峰 <zhourf@glodon.com>
2018-03-30 16:41:46 +0800

    修改配置

--- app/src/main/java/com/glodon/bim/business/greendao/GreenDaoHelper.java
+++ app/src/main/java/com/glodon/bim/business/greendao/GreenDaoHelper.java
@@ -1,54 +1,43 @@
 package com.glodon.bim.business.greendao;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 
+import com.glodon.bim.base.BaseApplication;
 import com.glodon.bim.gen.DaoMaster;
 import com.glodon.bim.gen.DaoSession;
 
 /**
- * 描述：
+ * 描述：数据库管理类
  * 作者：zhourf on 2017/9/14
  * 邮箱：zhourf@glodon.com
  */
 
 public class GreenDaoHelper {
 
-    private static DaoMaster.DevOpenHelper mHelper;
-    private static SQLiteDatabase db;
     private static DaoMaster mDaoMaster;
     private static DaoSession mDaoSession;
-
 
     /**
      * 初始化greenDao，这个操作建议在Application初始化的时候添加；
      */
     public static void initDatabase(Context context) {
-        // 通过 DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
-        // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为 greenDAO 已经帮你做了。
-        // 注意：默认的 DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
-        // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
-        mHelper = new DaoMaster.DevOpenHelper(context, "cache-db", null);
-        db = mHelper.getWritableDatabase();
+        DaoMaster.DevOpenHelper mHelper = new DaoMaster.DevOpenHelper(context, "cache-db", null);
         // 注意：该数据库连接属于 DaoMaster，所以多个 Session 指的是相同的数据库连接。
-        mDaoMaster = new DaoMaster(db);
+        mDaoMaster = new DaoMaster(mHelper.getWritableDatabase());
         mDaoSession = mDaoMaster.newSession();
     }
 
-//    private void initGreenDao() {
-//
-//        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "user.db", null);
-//
-//        //Database dataBase1 = helper.getWritableDb();
-//        Database database = helper.getEncryptedWritableDb("123");
-//
-//        daoSession=new DaoMaster(database).newSession();
-//    }
+
     public static DaoSession getDaoSession() {
+        if (mDaoSession == null) {
+            initDatabase(BaseApplication.getInstance());
+        }
         return mDaoSession;
     }
-    public static SQLiteDatabase getDb() {
-        return db;
+
+    public static DaoMaster getDaoMaster() {
+        return mDaoMaster;
     }
+
 
 }
